52f1291f4aa49d10bdb8d0e53b82ad4b27e1b0c7
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-04-25 20:28:15 +0200

    feat: create an initial empty rps project

---
+++ RPS/RPS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPS
+//
+//  Created by Michal Pietrzak on 25/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
