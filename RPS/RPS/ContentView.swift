86244b4ced25042f5688fafcfaefcdbcb4fbacb9
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-05-06 15:26:55 +0200

    feat: implement shadowStyle custom modifier

--- RPS/RPS/ContentView.swift
+++ RPS/RPS/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
+            
         }
+    }
+}
+
+struct Shadow: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 100, height: 100)
+            .background(.white)
+            .cornerRadius(20)
+            .shadow(
+                color: .gray.opacity(0.4),
+                radius: 9,
+                x: 0,
+                y: 0
+            )
+            .foregroundStyle(.black)
+            .font(.largeTitle)
+    }
+}
+
+extension View {
+    func shadowStyle() -> some View {
+        modifier(Shadow())
     }
 }
 
