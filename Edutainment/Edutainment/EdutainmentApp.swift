e05693cb5f41689309ea2d78e9a65bb99ccae3ef
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-10-29 12:16:31 +0100

    feat: create an initial empty edutainment project

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Michal Pietrzak on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
