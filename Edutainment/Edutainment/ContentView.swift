d2a83a93b80f4cdb76faf000f628c147853e9075
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-10-29 12:54:24 +0100

    feat: add 2 Steppers for input range + @State properties

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedNum1 = 0
+    @State private var selectedNum2 = 0
+    
     var body: some View {
         NavigationStack {
             Form {
                 Section("Select 2 numbers for difficulty range") {
+                    Stepper("\(selectedNum1)", value: $selectedNum1)
+                        .font(.headline).fontWeight(.bold)
                     
+                    Stepper("\(selectedNum2)", value: $selectedNum2)
+                        .font(.headline).fontWeight(.bold)
                 }
                 
                 Section("Select number of questions to be asked") {
