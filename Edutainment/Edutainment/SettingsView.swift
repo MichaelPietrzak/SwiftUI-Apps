ecbfc0a32e16e07a211d6ac0207077d17780306d
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-12-07 19:45:22 +0100

    feat: create an initial empty SettingsView

---
+++ Edutainment/Edutainment/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Edutainment
+//
+//  Created by Michal Pietrzak on 07/12/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
