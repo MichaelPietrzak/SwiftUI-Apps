32caa6abe51058b65e432fee6071bc54c585d560
Michał Pietrzak <michal.pietrzak18@gmail.com>
2025-05-14 15:23:33 +0200

    feat: create an initial empty habit track project

---
+++ Habit Track/Habit Track/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habit Track
+//
+//  Created by Michal Pietrzak on 14/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habit Track/Habit Track/Habit_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackApp.swift
+//  Habit Track
+//
+//  Created by Michal Pietrzak on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
